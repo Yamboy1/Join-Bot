82272a2e667de81fac4abd0d69e8e24a841a68d5
Yamboy1 <yamboyd1@gmail.com>
2021-08-15 04:06:09 +0000

    Add partial support for guildMemberRemove

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { log } from "./util/logger.js";
 import * as config from "./util/config.js";
 import * as events from "./events/index.js";
 
-const client = new Client({ intents: ["GUILD_MEMBERS", "GUILDS"] });
+const client = new Client({ intents: ["GUILD_MEMBERS", "GUILDS"], partials: ["GUILD_MEMBER"] });
 
 client.once('ready', () => {
 	log(`Bot running as ${client.user.tag} in ${client.guilds.cache.size} guilds.`);
