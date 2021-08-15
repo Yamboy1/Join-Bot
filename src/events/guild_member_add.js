d9685c65f46af27d7068fbc807d284643032ba93
Yamboy1 <yamboyd1@gmail.com>
2021-08-15 02:22:51 +0000

    Fix guild member events

--- src/events/guild_member_add.js
+++ src/events/guild_member_add.js
@@ -6,14 +6,16 @@ export const name = "guildMemberAdd";
 
 export async function handler(member) {
   const joinChannelId = db.getJoinChannel(member.guild.id);
+  if (!joinChannelId) return;
   const channel = await member.guild.channels.fetch(joinChannelId);
+  console.log(channel.id);
   if (!channel) return;
 
+
   const guild = await member.guild.fetch()
-  console.log(guild.memberCount);
 
   const embed = new MessageEmbed()
-    .setTitle(`Welcome ${member.displayName} To ${member.guild.name}`)
+    .setTitle(`Welcome ${member.displayName} To ${guild.name}`)
     .setColor(0x008000)
     .setDescription(`Welcome To ${guild.name}`)
     .addField("Time Joined:", member.joinedAt.toUTCString())
--- src/events/guild_member_remove.js
+++ src/events/guild_member_remove.js
@@ -6,13 +6,19 @@ export const name = "guildMemberRemove";
 
 export async function handler(member) {
   const joinChannelId = db.getJoinChannel(member.guild.id);
+  if (!joinChannelId) return;
   const channel = await member.guild.channels.fetch(joinChannelId);
+  console.log(channel.id);
   if (!channel) return;
-  embed = new MessageEmbed()
-    .setTitle(`${member.displayName} Left ${member.guild.name}`)
+
+  const guild = await member.guild.fetch();
+  console.log(guild.name, guild.memberCount)
+
+  const embed = new MessageEmbed()
+    .setTitle(`${member.displayName} Left ${guild.name}`)
     .setColor(0xFF0000)
     .setDescription("Cya")
-    .addField("Total Members", member.guild.memberCount)
+    .addField("Total Members", `${guild.memberCount}`)
     .setTimestamp()
     .setThumbnail(member.user.displayAvatarURL());
 
